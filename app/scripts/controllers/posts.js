4b3797c603aee06320e936a62369e3cdc5a2b9d8
Daniel Hollands <daniel@limeblast.co.uk>
2015-09-11 21:42:21 +0100

    security for the win

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,7 +1,8 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, $location, Post) {
+app.controller('PostsCtrl', function ($scope, $location, Auth, Post) {
   $scope.posts = Post.all;
+  $scope.user = Auth.user;
 
   $scope.post = {url: 'http://'};
 
--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -7,7 +7,11 @@ app.factory('Post', function ($firebase, FIREBASE_URL) {
   var Post = {
     all: posts,
     create: function (post) {
-      return posts.$add(post);
+      return posts.$add(post).then(function (postRef) {
+        $firebase(ref.child('user_posts').child(post.creatorUID))
+          .$push(postRef.name());
+        return postRef;
+      });
     },
     get: function (postId) {
       return $firebase(ref.child('posts').child(postId)).$asObject();
