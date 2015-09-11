c2a3aa9641de57597d7537d8233a5c6d25f7b69b
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-09-11 11:37:05 +0100

    adding and deleting - does my ability know no bound?

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,5 +3,15 @@
 /* global app:true */
 
 app.controller('PostsCtrl', function ($scope) {
+  $scope.posts = [];
   $scope.post = {url: 'http://', title: ''};
+
+  $scope.submitPost = function () {
+    $scope.posts.push($scope.post);
+    $scope.post = {url: 'http://', title: ''};
+  };
+
+  $scope.deletePost = function (index) {
+    $scope.posts.splice(index, 1);
+  };
 });
