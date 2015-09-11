0af3fe45100ab788fef8981b5fe0721a4fa32104
Daniel Hollands <daniel@limeblast.co.uk>
2015-09-11 21:36:05 +0100

    stuff, again

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -8,6 +8,8 @@ app.controller('NavCtrl', function ($scope, $location, Auth, Post) {
   $scope.user = Auth.user;
 
   $scope.submitPost = function () {
+    $scope.post.creator = $scope.user.profile.username;
+    $scope.post.creatorUID = $scope.user.uid;
     Post.create($scope.post).then(function (ref) {
       $location.path('/posts/' + ref.name());
       $scope.post = {url: 'http://', title: ''};
