10954423a754e9aba0a886125c3cd03e8028fa30
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-09-11 17:07:50 +0100

    sexhy

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,12 +5,6 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
 
   $scope.post = {url: 'http://'};
 
-  $scope.submitPost = function () {
-    Post.create($scope.post).then(function (ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
-
   $scope.deletePost = function (post) {
     Post.delete(post);
   };
